721814de45cd41333b939ec1c89220ada8e83a98
AndriyHronskyi <Andrii.Hronskyi.work@gmail.com>
2022-07-11 18:02:07 +0300

    Create extended library for collections of algorithms

---
+++ Algorithm/AlgorithmBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class AlgorithmBase<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+
+        public void Swop(int positionA, int positionB)
+        {
+            if (positionA < Items.Count && positionB < Items.Count)
+            {
+
+            }
+        }
+    }
+}
