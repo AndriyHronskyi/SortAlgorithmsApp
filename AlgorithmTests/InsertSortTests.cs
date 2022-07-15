48d1ffd95b7c237dd11556c6cc31b93c6963bed5
AndriyHronskyi <Andrii.Hronskyi.work@gmail.com>
2022-07-15 15:22:32 +0300

    Lots of changes. 1) Add Insert sort algorithm. 2) Add InsertSortTests which wood renamed into SortTests. 3)  InsertSortTests (SortTests) wood be a TestClass with test initialize. Now, to testing all algorithms. List Items and List Sorted (items) and Ramdom rnd -  initialize one time and using multiple times for every test algorithm  4) InsertSortTests (SortTests) now realize buble sort test. Another tests wood added shortly

---
+++ AlgorithmTests/InsertSortTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class SortTests
+    {
+        Random rnd = new Random();
+        List<int> Items = new List<int>();
+        List<int> Sorted = new List<int>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            Items.Clear();
+            for (int i = 0; i < 1000; i++)
+            {
+                Items.Add(rnd.Next(0, 100));
+            }
+
+            Sorted.Clear();
+            Sorted.AddRange(Items.OrderBy(x => x).ToArray());
+        }
+
+        [TestMethod()]
+        public void SubleTest()
+        {
+            // arrange
+            var buble = new BubleSort<int>();
+            buble.Items.AddRange(Items);
+
+            // act
+            buble.Sort();
+
+            // assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Sorted[i], buble.Items[i]);
+            }
+
+        }
+    }
+}
